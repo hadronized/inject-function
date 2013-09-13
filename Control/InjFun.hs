a3124429eeae862acdc2cf97964d7fddd39eb1bd
Dimitri Sabadie <dimitri.sabadie@me.com>
2013-09-13 10:26:24 +0200

    Moved old (|->) into (||->) and add correct (|->).

--- Control/InjFun.hs
+++ Control/InjFun.hs
@@ -5,45 +5,53 @@ module Control.InjFun (
   , inject
     -- * Sequencing, exploding and merging
   , (|->)
+  , (||->)
   , explode
   , merge
   ) where
 
 -- |Function able to be injected parameters in.
--- `i` represents its input, `c` is the injected control parameters, `m` is the resulting monad
+-- `c` is the injected control parameters, `i` represents its input, `m` is the resulting monad
 -- and `o` is the output.
-newtype InjFun i c m o = InjFun {
+newtype InjFun c i m o = InjFun {
   -- |Feed a `InjFun` with its regular parameters and injected parameters.
-  cfapply :: i -> c -> m o
+  cfapply :: c -> i -> m o
   }
 
 -- |Create an inject function.
-inject :: (i -> c -> m o) -> InjFun i c m o
+inject :: (c -> i -> m o) -> InjFun c i m o
 inject f = InjFun f
 
 -- |Sequencing operator. It’s a helper function that composes with `>>=` the two `InjFun`, respecting
--- the order.
-(|->) :: (Monad m) => InjFun i c m o       -- ^ First function
-                   -> InjFun o c' m o'     -- ^ Second function
-                   -> InjFun i (c,c') m o' -- ^ Resulting sequencing function
-f |-> g = InjFun $ \i (c,c') -> do
-  r0 <- cfapply f i c
-  cfapply g r0 c'
+-- the order. That version (with a single `|`) means that both the two injected parameters are considered
+-- the same; then they’re shared as a single `c`.
+(|->) :: (Monad m) => InjFun c i m o  -- ^ First function
+                   -> InjFun c o m o' -- ^ Second function
+                   -> InjFun c i m o' -- ^ Resulting sequencing function
+f |-> g = InjFun $ \c i -> cfapply f c i >>= cfapply g c
+
+-- |Sequencing operator. It’s a helper function that composes with `>>=` the two `InjFun`, respecting
+-- the order. That version (with double `|`) means that the two injected parameters are considered
+-- different.
+(||->) :: (Monad m) => InjFun c i m o       -- ^ First function
+                    -> InjFun c' o m o'     -- ^ Second function
+                    -> InjFun (c,c') i m o' -- ^ Resulting sequencing function
+f ||-> g = InjFun $ \(c,c') i -> cfapply f c i >>= cfapply g c'
 
 -- |Explode an `InjFun` that outputs two values into two other `InjFun`.
-explode :: (Monad m) => InjFun i c m (o0,o1)              -- ^ Function to explode
-                     -> (InjFun i c m o0,InjFun i c m o1) -- ^ Exploded functions
+explode :: (Monad m) => InjFun c i m (o0,o1)              -- ^ Function to explode
+                     -> (InjFun c i m o0,InjFun c i m o1) -- ^ Exploded functions
 explode f = (f',f'')
   where f'  = cf fst
         f'' = cf snd
-        cf sel = InjFun $ \i c -> cfapply f i c >>= return . sel
+        cf sel = InjFun $ \c i -> cfapply f c i >>= return . sel
 
 -- |Merge two `InjFun` into one.
-merge :: (Monad m) => InjFun i c m o                -- ^ First function
-                   -> InjFun i' c' m o'             -- ^ Second function
-                   -> InjFun (i,i') (c,c') m (o,o') -- ^ Merged function
+merge :: (Monad m) => InjFun c i m o                -- ^ First function
+                   -> InjFun c' i' m o'             -- ^ Second function
+                   -> InjFun (c,c') (i,i') m (o,o') -- ^ Merged function
 merge f g = fg
-  where fg = InjFun $ \(i,i') (c,c') -> do
-                r0 <- cfapply f i c
-                r1 <- cfapply g i' c'
+  where fg = InjFun $ \(c,c') (i,i') -> do
+                r0 <- cfapply f c i
+                r1 <- cfapply g c' i'
                 return (r0,r1)
